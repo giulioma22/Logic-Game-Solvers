3aa4e42fc3f54afb70dc31f3b4e972dfdc723ade
giulioma22 <giuliomartinelli22@gmail.com>
2019-09-21 19:20:14 +0200

    Initial grid is done

--- Abc.py
+++ Abc.py
@@ -4,18 +4,9 @@ import math
 
 size = 4
 letters = 3
-alph_order = ["A", "B", "C", "D", "E"]
-order = [3, 2, 3, 1, 1, 2, 2, 3, 3, 1, 3, 2, 2, 1, 1, 3]
-
-# def draw_grid(size):
-#     for i in range(size):
-#         print("+ - - - " * size + "+ \n" + ("|   " + str(letters) + "   ") * size + "|" )
-#     print("+ - - - " * size + "+")
-#     return
-
-# draw_grid(4)
-
-# grid = [[".", "A", "B", "C"], ["A", ".", "B", "C"], ["A", "B", ".", "C"], ["A", "B", "C", "."]]
+alph_order = ["A", "B", "C", "D", "E", "-"]
+hor_order = [2, 1, 2, 0, 2, 0, 2, 1]
+ver_order = [2, 0, 0, 1, 2, 1, 1, 0]
 
 #Used to fill cells at the beginning
 
@@ -23,7 +14,10 @@ init_fill = ""
 
 for i in range(letters):
     init_fill += alph_order[i]
-print(init_fill)
+
+# init_array = []
+# for i in range(size+2):
+#     init_array.append(init_fill)
 
 #Create the playing grid
 grid = []
@@ -33,11 +27,20 @@ for i in range(size+2):
         if i != 0 and i != 5 and j != 0 and j != 5:
             line.append(init_fill)
         else:
-            line.append("---")
+            line.append("___")
     grid.append(line)
 
-grid[0][1] = "_A_"
+#Add the side letters
+for i in range(size):
+    grid[0][i+1] = "_" + alph_order[hor_order[i]] + "_"
+    grid[size+1][i+1] = "_" + alph_order[hor_order[-i-1]] + "_"
+    grid[i+1][0] = "_" + alph_order[ver_order[i]] + "_"
+    grid[i+1][size+1] = "_" + alph_order[ver_order[-i-1]] + "_"
 
 #Drawing the grid
 for i in range(size+2):
     print(grid[i])
+
+#Heuristic function
+
+
